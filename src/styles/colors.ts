a8f36e469c0a1e535e67dbe0f546c8f1bf3be75c
MJ <alswns8495@naver.com>
2025-07-24 23:40:08 +0900

    🎨 : 프로젝트 전역 색상 상수(colors.ts) 추가

---
+++ src/styles/colors.ts
@@ -0,0 +1,29 @@
+// src/styles/colors.ts
+export const Colors = {
+  // Primary
+  primary_900: "#100400",
+  primary_800: "#431200",
+  primary_700: "#A92D00",
+  primary_600: "#DC3B00",
+  primary_500: "#F71172",
+  primary_400: "#FF7543",
+  primary_300: "#FF9B76",
+  primary_200: "#FFAD8F",
+  primary_100: "#FFD3C2",
+
+  // Grayscale
+  gray_900: "#0D0D0D",
+  gray_800: "#262626",
+  gray_700: "#333333",
+  gray_600: "#4D4D4D",
+  gray_500: "#666666",
+  gray_400: "#808080",
+  gray_300: "#999999",
+  gray_200: "#D9D9D9",
+  gray_100: "#E6E6E6",
+
+  // Palette
+  black: "#000000",
+  white: "#FFFFFF",
+  brandingRedOrange: "#F71172", // 이미지상 Branding Red Orange (spot 브랜드 색)
+};
