ab88a002eb5d04ae91d2cb8b57edb926f88aa537
MJ <alswns8495@naver.com>
2025-07-27 16:15:08 +0900

    ✨ 앱 실행 시 위치 권한 요청 및 내 위치 추적 버튼 구현

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,30 @@
+import React, { useEffect, useRef } from "react";
 import { View, StyleSheet } from "react-native";
 import { NaverMapView } from "@mj-studio/react-native-naver-map";
+import type { NaverMapViewRef } from "@mj-studio/react-native-naver-map";
+import * as Location from "expo-location";
 
 export default function Home() {
+  const mapRef = useRef<NaverMapViewRef>(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status === "granted") {
+        mapRef.current?.setLocationTrackingMode("Follow");
+      } else {
+        console.log("위치 권한이 거부되었습니다.");
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <NaverMapView style={styles.map}></NaverMapView>
+      <NaverMapView
+        ref={mapRef}
+        isShowLocationButton={true}
+        style={styles.map}
+      />
     </View>
   );
 }
